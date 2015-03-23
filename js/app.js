3038ae9f27489a035cae5ed79f0aafe3543e86f5
Cruel <machin3@gmail.com>
2015-03-23 02:56:00 -0400

    Add social media sharing and pagination

--- js/app.js
+++ js/app.js
@@ -6,12 +6,14 @@ $(function(){
         $('.top-bar form').submit(function(e){
             e.preventDefault();
         });
-        $('#search-term').text(location.search);
+        $('#search-term').text(location.search.slice(3));
         $('#search-query').lunrSearch({
             indexUrl: '/search.json',             // URL of the `search.json` index data for your site
             results:  '#search-results',          // jQuery selector for the search results container
             entries:  '.entries',                 // jQuery selector for the element to contain the results list, must be a child of the results element above.
             template: '#search-results-template'  // jQuery selector for the Mustache.js template
-        });
+        }).keyup(function(){
+            $('#search-term').text($(this).val());
+        });;
     }
 });
--- tags/egoism.md
+++ tags/egoism.md
@@ -1,5 +1,7 @@
 ---
+title: Egoism Tagged Posts
 layout: blog_by_tag
 tag: egoism
 permalink: tag/egoism/
+exclude_from_search: true
 ---
